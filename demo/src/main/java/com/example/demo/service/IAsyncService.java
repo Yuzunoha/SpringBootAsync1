703fa310d5f1c787ddc23b31d167d2de8e17df3c
Yuzunoha <studysteadystandby@gmail.com>
2023-04-02 18:15:52 +0900

    削除

--- demo/src/main/java/com/example/demo/service/IAsyncService.java
+++
@@ -1,9 +0,0 @@
-package com.example.demo.service;
-
-import java.util.concurrent.CompletableFuture;
-
-public interface IAsyncService {
-  CompletableFuture<String> method1();
-  CompletableFuture<String> method2();
-  CompletableFuture<String> method3();
-}
--- demo/src/main/java/com/example/demo/service/IStringService.java
+++
@@ -1,7 +0,0 @@
-package com.example.demo.service;
-
-public interface IStringService {
-  String method1();
-  String method2();
-  String method3();
-}
